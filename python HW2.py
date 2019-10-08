b3558cbe02639d20fc96b0b6ada0b2391d8d1d1d
oykuzumrutdal <55995949+oykuzumrutdal@users.noreply.github.com>
2019-10-08 19:02:21 +0300

    Add files via upload

---
+++ python HW2.py
@@ -0,0 +1,10 @@
+age = 23
+if age < 1:
+    print ('Oyku is an infant. ')
+elif age >= 0 and age < 10:
+    print ( 'Oyku is a child. ')
+elif age >= 10 and age < 19:
+    print (' Oyku is a child and adolescent. ')
+else:
+    print (' Oyku is an adult.' )
+git.S/Q
